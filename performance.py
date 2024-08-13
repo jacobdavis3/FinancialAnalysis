7480bc80c9924fd1fa2f6acab581b4c3fce3112a
Jacob Davis <jd3728@princeton.edu>
2024-08-13 12:38:31 -0400

    cleaning things up

--- performance.py
+++ performance.py
@@ -5,12 +5,6 @@ import yfinance as yf
 class Stock:
     ...
 
-history_periods = ['1d', '5d', '1mo', '3mo', '6mo', 
-                     '1y', '2y', '5y', '10y', 'ytd', 'max'
-                     ]
-arm = yf.Ticker("ARM")
-#his = arm.history(period='5y')
-
 
 def calc_percentage(stock):  # figure out how to do type annotation
     history_periods = ['1d', '5d', '1mo', '3mo', '6mo', 
@@ -18,29 +12,19 @@ def calc_percentage(stock):  # figure out how to do type annotation
                      ]
     for period in history_periods:
         timeframe = stock.history(period=period)
-        print(timeframe['Open'].iloc[0], period, sep=' - ')
-        print(timeframe['Close'].iloc[-1], period, sep=' - ')
+        first_price = timeframe['Open'].iloc[0]
+        last_price = timeframe['Close'].iloc[-1]
+        difference = last_price - first_price
+        percent_change = difference / first_price * 100
+        print(f'{percent_change}% - {period}')
 
 
+def main():
+    msft = yf.Ticker("MSFT") 
+    sp500 = yf.Ticker("^GSPC")            
+    calc_percentage(msft)
+    calc_percentage(sp500)
 
 
-msft = yf.Ticker("MSFT")             
-
-calc_percentage(msft)
-########## What happens if I call 10y on a stock w/o that longevity, like ARM ####
-## ARM: Period '5y' is invalid, must be one of ['1d', '5d', '1mo', '3mo', '6mo', '1y', 'ytd', 'max']
-## Empty DataFrame
-
-#arm = yf.Ticker("ARM")
-#his = arm.history(period='5y')
-#print(arm.history(period='5y'))
-
-
-
-msft_info = msft.info
-#print(msft.history(period='5y'))
-
-
-
-sp500 = yf.Ticker("^GSPC")
-#print(sp500.info)
+if __name__ == "__main__":
+    main()
