113e7286d3874525aded113624251706c4fe245d
Jacob Davis <jd3728@princeton.edu>
2024-08-13 16:46:53 -0400

    works for all ranges, so even day after IPO

--- performance.py
+++ performance.py
@@ -1,12 +1,16 @@
 import yfinance as yf
 
 
-
 class Stock:
-    ...
+    def __init__(self, ticker:str) -> None:
+        self.attributes = yf.Ticker(ticker)
+
+    def validRanges(self) -> None:
+        ranges = self.attributes.history_metadata['validRanges']
+        print(ranges)
 
 
-def calc_percentage(stock):  # figure out how to do type annotation
+def calc_percentage(stock:yf): 
     history_periods = ['1d', '5d', '1mo', '3mo', '6mo', 
                      '1y', '2y', '5y', '10y', 'ytd', 'max'
                      ]
@@ -15,15 +19,22 @@ def calc_percentage(stock):  # figure out how to do type annotation
         first_price = timeframe['Open'].iloc[0]
         last_price = timeframe['Close'].iloc[-1]
         difference = last_price - first_price
-        percent_change = difference / first_price * 100
-        print(f'{percent_change}% - {period}')
+        percent_change = round(difference / first_price * 100, 2)
+        percent_change_string = "{:,}".format(percent_change) + '%'
+        print(f'{percent_change_string} - {period}')
 
 
 def main():
-    msft = yf.Ticker("MSFT") 
-    sp500 = yf.Ticker("^GSPC")            
-    calc_percentage(msft)
-    calc_percentage(sp500)
+    #msft = yf.Ticker("MSFT") 
+    #hist = msft.history()
+    #value = msft.history_metadata
+    #print(value)
+    #sp500 = yf.Ticker("^GSPC")            
+    #calc_percentage(msft)
+    #calc_percentage(sp500)
+    msft = Stock("msft")
+    msft.validRanges()
+    #print(msft.attributes.history())
 
 
 if __name__ == "__main__":
